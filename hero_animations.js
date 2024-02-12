cfceb28c4b159a4d397d267c68fc6b42c9cdb9d3
Mthobisi-za <mthobisimtho333@gmail.com>
2024-02-12 21:21:47 +0200

    done

--- hero_animations.js
+++ hero_animations.js
@@ -32,6 +32,15 @@ gsap.from('.hero_img', {
     rotate: 10,
     ease: "power1.inOut"
 });
+gsap.from('.skills_2', {
+    scrollTrigger: ".express_js",
+    // opacity: 0,
+    duration: 5,
+    // scale: 0,
+    ease: "power1.inOut",
+    width: 0,
+    opacity: 0
+});
 
 // gsap.to(".hero_img", {
 //     scrollTrigger: ".btn_wrapper_long", // start the animation when ".box" enters the viewport (once)
